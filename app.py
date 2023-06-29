2a244c592b0c3a4b3fa2e0be0c27e04f33240660
sahayana <saokhueyang@gmail.com>
2023-06-29 19:23:56 +0900

    추가: rds + secret manager checking

--- app.py
+++ app.py
@@ -1,24 +1,24 @@
 import time
-import redis
+
 from flask import Flask
 
 
 app = Flask(__name__)
-db_cache = redis.Redis(host='redis', port=6379)
+# db_cache = redis.Redis(host='redis', port=6379)
 
 
-def web_hit_cnt():
-    return db_cache.incr('hits')
+# def web_hit_cnt():
+#     return db_cache.incr('hits')
 
 
-@app.route('/')
-def index():
+# @app.route('/')
+# def index():
 
-    cnt = web_hit_cnt()
-    return '''
-    <h1 style=color:deepskyblue;>docker-compose application</h1>
-    <p style=color:deepskyblue;>web access count: {} times</p>
-    '''.format(cnt)
+#     cnt = web_hit_cnt()
+#     return '''
+#     <h1 style=color:deepskyblue;>docker-compose application</h1>
+#     <p style=color:deepskyblue;>web access count: {} times</p>
+#     '''.format(cnt)
 
 
 if __name__ == "__main__":
